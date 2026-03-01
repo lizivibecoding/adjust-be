50e8434b572ca7268d6bd44e62b0363a8e5f9ca5
hu <hu__fx@163.com>
2026-03-01 17:59:01 +0800

    fix(调剂推荐)：返回科目信息

--- adjust-module-biz/src/main/java/com/hongguoyan/module/biz/controller/admin/userprofile/vo/UserProfileRespVO.java
+++ adjust-module-biz/src/main/java/com/hongguoyan/module/biz/controller/admin/userprofile/vo/UserProfileRespVO.java
@@ -1,6 +1,7 @@
 package com.hongguoyan.module.biz.controller.admin.userprofile.vo;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.math.BigDecimal;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -14,5 +15,42 @@ public class UserProfileRespVO extends UserProfileSaveReqVO {
 
     @Schema(description = "创建时间", requiredMode = Schema.RequiredMode.REQUIRED)
     private LocalDateTime createTime;
+
+    /**
+     * 科目1代码
+     */
+    private String subjectCode1;
+    /**
+     * 科目1名称
+     */
+    private String subjectName1;
+
+    /**
+     * 科目2代码
+     */
+    private String subjectCode2;
+    /**
+     * 科目2名称
+     */
+    private String subjectName2;
+
+    /**
+     * 科目3代码
+     */
+    private String subjectCode3;
+    /**
+     * 科目3名称
+     */
+    private String subjectName3;
+
+    /**
+     * 科目4代码
+     */
+    private String subjectCode4;
+    /**
+     * 科目4名称
+     */
+    private String subjectName4;
+
 
 }
